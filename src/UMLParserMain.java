3f52c6e38087f50e97d128449b5fa7f8604098a8
snehalphatangare <snehalphatangare@gmail.com>
2017-03-01 08:27:55 -0800

    Created UMLParserMain.java
    Application invocation file.

---
+++ src/UMLParserMain.java
@@ -0,0 +1,77 @@
+import java.io.*;
+
+import java.util.*;
+
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.TypeDeclaration;
+
+public class UMLParserMain {
+	private static String srcFolderName;
+	private static String outputFileName;
+	public static Hashtable<String, ClassInformation> mapClassNameToInfo;
+	public static ArrayList<ClassInformation> lstClasses;
+
+	public static void main(String[] args) throws Exception {
+		mapClassNameToInfo = new Hashtable<String, ClassInformation>();
+		lstClasses = new ArrayList<ClassInformation>();
+		
+/*		System.out.println("Program Arguments:");
+		if(args.length > 0){
+			if(args[0]!=null && args[0]!=""){
+				srcFolderName=args[0];
+			}else{
+				System.out.println("Please provide Source folder argument");
+				System.exit(1);
+			}
+			System.out.println("Source Folder \t" + srcFolderName);
+			
+			if(args[1]!=null && args[1]!=""){
+				outputFileName=args[1];
+			}else{
+				System.out.println("Please provide Output file name as argument");
+				System.exit(1);
+			}
+			System.out.println("Output File argument \t" + outputFileName);
+		}*/
+		
+		
+		//HARDCODED ARGUMENTS
+		srcFolderName="C:/Semester I/CMPE202/cmpe202-master/umlparser/uml-parser-test-2";
+		
+		//Process all .java files in srcFolder
+		File folder = new File(srcFolderName);
+		for(File fileObj:folder.listFiles()){
+			if(fileObj.getName().endsWith(".java")){
+				System.out.println("Processing file="+fileObj.getName());
+				// creates an input stream for the file to be parsed
+				FileInputStream in = new FileInputStream(fileObj);
+				// parse file
+				CompilationUnit cu = JavaParser.parse(in);
+				
+				// visit and print the class names
+				List<TypeDeclaration> types =cu.getTypes();
+				for (TypeDeclaration type : types) {
+					if (type instanceof ClassOrInterfaceDeclaration) {
+						ClassOrInterfaceDeclaration classDec = (ClassOrInterfaceDeclaration) type;
+						ClassInformation c=new ClassInformation().getClassInformation(classDec);
+						lstClasses.add(c);
+						mapClassNameToInfo.put(c.name, c);
+						System.out.println("********class info= "+c.name+" is interface= "+c.isInterface + " inherited classes= "+c.lstInheritedClasses+" implemented classes= "+c.lstImplementedClasses);
+					}
+				}
+				
+				
+			}
+		}
+		
+		//Form Plant UML generator Input
+		UMLGeneratorInputCreator u=new UMLGeneratorInputCreator();
+		u.formGeneratorString(mapClassNameToInfo);
+		
+		//Call to PlantUML
+
+	}
+
+}
