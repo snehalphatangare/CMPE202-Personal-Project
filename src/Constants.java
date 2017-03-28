f8c90a90b67c3133ec519e9a6501ed87ab6c0e05
snehalphatangare <snehalphatangare@gmail.com>
2017-03-28 11:58:28 -0700

    Update Constants.java

--- src/Constants.java
+++ src/Constants.java
@@ -2,4 +2,5 @@
 public final class Constants {
 	public static final String privateModifier = "PRIVATE";
 	public static final String publicModifier = "PUBLIC";
+	public static final String protectedModifier = "PROTECTED";
 }
