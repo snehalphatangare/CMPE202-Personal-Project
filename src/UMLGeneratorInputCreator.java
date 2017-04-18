e0c5861ae2a41c3f0472fca2ce763661b561df9e
snehalphatangare <snehalphatangare@gmail.com>
2017-04-17 19:31:10 -0700

    Update UMLGeneratorInputCreator.java

--- src/UMLGeneratorInputCreator.java
+++ src/UMLGeneratorInputCreator.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 import japa.parser.ast.type.ClassOrInterfaceType;
+import japa.parser.ast.type.ReferenceType;
 import net.sourceforge.plantuml.SourceStringReader;
 
 public class UMLGeneratorInputCreator {
@@ -17,7 +18,7 @@ public class UMLGeneratorInputCreator {
 		return UMLGeneratorInp;
 	}
 	
-	public void formGeneratorString(Hashtable<String, ClassInformation> mapClassNameToInfo,ArrayList<RelationshipInformation> lstRelDetails) throws IOException{
+	public void formGeneratorString(Hashtable<String, ClassInformation> mapClassNameToInfo,ArrayList<RelationshipInformation> lstRelDetails,String outputFileName) throws IOException{
 		this.lstClasses= new ArrayList<ClassInformation> (mapClassNameToInfo.values());
 		
 		UMLGeneratorInp = "@startuml\n";
@@ -25,15 +26,21 @@ public class UMLGeneratorInputCreator {
 		
 		//ADD RELATIONSHIP DETAILS
 		if(lstRelDetails.size()>0){
-			for(RelationshipInformation r: lstRelDetails){
-				//UMLGeneratorInp +=c.name+" --|> "+cls.getName()+"\n";
-				UMLGeneratorInp +=r.getSrcCls()+" "+r.getRel()+" "+r.getDestCls()+"\n";
+			for(RelationshipInformation r:lstRelDetails){
+				System.out.println("+++++++++ "+r.getSrcCls()+" "+r.getDestCls()+" "+r.getSrcMultiplicity()+" "+r.getDestMultiplicity());
 			}
+			for(RelationshipInformation r: lstRelDetails){
+				String srcMul=r.getSrcMultiplicity()!= null ? "\""+r.getSrcMultiplicity()+"\""+" ":"";
+				String destMul=r.getDestMultiplicity()!= null ? "\""+r.getDestMultiplicity()+"\""+" ":"";
+				UMLGeneratorInp +=r.getSrcCls()+" "+ srcMul+
+									r.getRel()+" "+ destMul+
+									r.getDestCls()+"\n";
+			}
+			System.out.println("**********UMLGeneratorInp="+UMLGeneratorInp);
 		}
 
 		
 		for(ClassInformation c: this.lstClasses){
-			//System.out.println("********in uml generator class info= "+c.name+" is interface= "+c.isInterface + " inherited classes= "+c.lstInheritedClasses+" implemented classes= "+c.lstImplementedClasses);
 			//Add interface declarations
 			if(c.isInterface)
 				UMLGeneratorInp +="interface "+c.name+"\n";
@@ -46,20 +53,28 @@ public class UMLGeneratorInputCreator {
 			
 			//Attribute Details
 			for(AttributeInformation a:  c.mapAttributes.values()){
-				//Access specifier
-				if(a.getAccessSpecifier() == Constants.privateModifier)
-					UMLGeneratorInp +="- ";
-				else if(a.getAccessSpecifier() == Constants.publicModifier)
-					UMLGeneratorInp +="+ ";
-				
-				//Attribute Name
-				UMLGeneratorInp +=a.getName();
-				
-				//Attribute data type
-				if(a.getType()!=null && a.getType().toString()!=null)
-					UMLGeneratorInp +=" : "+a.getType().toString()+"\n";
-				else
-					UMLGeneratorInp +="\n";
+				//Only Show Public and Private and Primitive type Attributes 
+				if((a.getAccessSpecifier() == Constants.privateModifier || a.getAccessSpecifier()== Constants.publicModifier))
+				{
+					//if(a.getType()!=null && (a.getType().toString().equalsIgnoreCase("String") || a.getType().toString().contains("[") || !(a.getType() instanceof ReferenceType)))
+					if(a.getType()!=null && !Utility.isReferenceTypeVariable(a.getType()))
+					{
+						//Access specifier
+						if(a.getAccessSpecifier() == Constants.privateModifier)
+							UMLGeneratorInp +="- ";
+						else if(a.getAccessSpecifier() == Constants.publicModifier)
+							UMLGeneratorInp +="+ ";
+						
+						//Attribute Name
+						UMLGeneratorInp +=a.getName();
+						
+						//Attribute data type
+						if(a.getType()!=null && a.getType().toString()!=null)
+							UMLGeneratorInp +=" : "+a.getType().toString()+"\n";
+						else
+							UMLGeneratorInp +="\n";
+					}
+				}
 			}
 			
 			//Method details
@@ -98,16 +113,13 @@ public class UMLGeneratorInputCreator {
 		UMLGeneratorInp +=" @enduml\n";
 		System.out.println("Final generator input= "+UMLGeneratorInp);
 		
-		OutputStream png = new FileOutputStream("C:/Users/Snehal Phatangare/Pictures/testImg.png");
-		/*UMLGeneratorInp = "@startuml\n";
-		UMLGeneratorInp +="Class01 <|-- Class02\n" ;
-		UMLGeneratorInp +="Class03 <|-- Class04\n" ;
-		UMLGeneratorInp +="@enduml\n";*/
+		//OutputStream png = new FileOutputStream("C:/Users/Snehal Phatangare/Pictures/testImg");
+		OutputStream png = new FileOutputStream(outputFileName+".png");
 		
 		SourceStringReader reader = new SourceStringReader(UMLGeneratorInp);
-		// Write the first image to "png"
-		String desc = reader.generateImage(png);
-		System.out.println("Done!");
+		// Write the image to "png"
+		reader.generateImage(png);
+		System.out.println("Done! ");
 		
 		
 	}
